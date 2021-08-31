887d864a851288e2392de17f81dbb9e6dd09ee92
Xarria <224340@edu.p.lodz.pl>
2021-08-31 19:50:10 +0200

    Initial commit

---
+++ backend/HospitalWard/src/main/java/com/backend/hospitalward/HospitalWardApplication.java
@@ -0,0 +1,13 @@
+package com.backend.hospitalward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalWardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalWardApplication.class, args);
+    }
+
+}
---
+++ backend/HospitalWard/src/test/java/com/backend/hospitalward/HospitalWardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.hospitalward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalWardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
