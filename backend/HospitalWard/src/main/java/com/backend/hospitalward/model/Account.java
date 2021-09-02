450dda19d3aedbbba86a2268bb912a9d44486a7f
Xarria <224340@edu.p.lodz.pl>
2021-09-02 19:53:51 +0200

    Add liquibase changelogs

--- backend/HospitalWard/src/main/java/com/backend/hospitalward/model/Account.java
+++ backend/HospitalWard/src/main/java/com/backend/hospitalward/model/Account.java
@@ -1,4 +1,18 @@
 package com.backend.hospitalward.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Account {
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
