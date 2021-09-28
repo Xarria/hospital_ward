3724b882303748cfa6b75b7606eb80d9d8127f87
Xarria <224340@edu.p.lodz.pl>
2021-09-28 13:41:25 +0200

    Add account dto

---
+++ backend/HospitalWard/src/main/java/com/backend/hospitalward/dto/AccountGeneralResponse.java
@@ -0,0 +1,17 @@
+package com.backend.hospitalward.dto;
+
+import com.backend.hospitalward.model.AccessLevel;
+
+public class AccountGeneralResponse {
+
+    String login;
+
+    String type;
+
+    AccessLevel accessLevel;
+
+    String name;
+
+    String surname;
+
+}
